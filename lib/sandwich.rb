26089ec5aa39d2e32a8160e4d328afb8c20df300
Weinan Li <l.weinan@gmail.com>
2019-05-17 20:06:31 +0800

    fix bugs

--- lib/sandwich.rb
+++ lib/sandwich.rb
@@ -1,4 +1,7 @@
-class Bowling
+class Sandwich
   attr_accessor :taste
 
+  def initialize(taste)
+    @taste = taste
+  end
 end
--- spec/sandwhich_spec.rb
+++ spec/sandwhich_spec.rb
@@ -2,9 +2,9 @@ require 'sandwich'
 
 RSpec.describe '三明治' do
   it '好吃' do
-    sandwich = Sandwich.new("真香", [])
+    sandwich = Sandwich.new("真香")
     taste = sandwich.taste
-    expect(taste).do eq('真香')
+    expect(taste).to eq('真香')
   end
 end
 
